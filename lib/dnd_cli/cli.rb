57c5ba1e56a7a247a8cd215437b860a7c1c4ff6d
jhobgoo <jhobgoo@gmail.com>
2020-04-13 22:14:32 -0400

    clean up comments

--- lib/dnd_cli/cli.rb
+++ lib/dnd_cli/cli.rb
@@ -65,36 +65,3 @@ class CLI
   end
 
 end
-
-  # def select_spell
-  #   puts "[Enter the corresponding (number) of the spell for which you would like Zed to give you more information, or you may type exit to leave Zed's shop!]"
-  #   puts ">"
-  #
-  #   user_input = gets.strip.to_i
-  #   spell_object = Spell.all[user_input - 1]
-  #
-  #     if self.valid_input?(user_input) == 'exit'
-  #       puts "Of course, of course! Zed humbly thanks you for visiting his spell shop. Safe travels, adventurer!"
-  #       exit
-  #     elsif !self.valid_input?(user_input)
-  #       puts "I deeply apologize adventurer, but it appears that spell is not on my list. Come, let me tell you about another spell on my list instead:\n\n"
-  #       sleep(4)
-  #       self.select_spell
-  #     else
-  #       API.get_descriptions(spell_object)
-  #     end
-  #
-  #   puts "Ahhh yes, #{spell_object.name} is it? A formidable spell in the hands of the right weilder, yes... Here's what I know about this spell:\n\n"
-  #     spell_object.description.each do |element|
-  #       puts "#{element}\n"
-  #     end
-  #
-  #     if spell_object.material != nil
-  #       puts "To cast this spell requires #{spell_object.material.downcase}\n\n"
-  #     end
-  #
-  #   sleep(12) ##sleep timer to allow for reading of spell_description output before looping back to self.select_spell
-  #
-  #   self.select_spell
-  #
-  # end
